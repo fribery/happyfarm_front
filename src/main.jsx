36e3fce246f82d94b748ed5fa624efa5add80678
fribery <fribery.blog@gmail.com>
2025-12-18 00:12:34 +0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Debug from './debug'; // Импорт вашего основного компонента
+import App from './App'; // Импорт вашего основного компонента
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Debug /> {/* Рендерим ваш App.jsx */}
+    <App /> {/* Рендерим ваш App.jsx */}
   </React.StrictMode>
 );
